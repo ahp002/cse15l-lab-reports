255a770fc72ff16e786160aecc418f455099f432
ahp002 <aliciap.ap6@gmail.com>
2023-04-24 21:06:32 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,27 +1,32 @@
 import java.io.IOException;
 import java.net.URI;
 
-class Handler implements URIHandler{
+class ServerHandler implements URLHandler {
+
+    String message = "";
 
     public String handleRequest(URI url) {
-        if(url.getPath().equals("/add-message?s=")) {
-            String message = 
-            return 
+        if (url.getPath().equals("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            message += parameters[1] + "\n";
+            
+            return String.format(message);
         }
+        return "404 Not Found!";
     }
+    
 
 }
 
 class StringServer {
-
-    public static void main(String args[]) throws IOException {
-        if(args.length == 0) {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new Handler());
+        Server.start(port, new ServerHandler());
     }
 }
